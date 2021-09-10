54c65cd681f378fead072591a71f8576454bb3ff
Peter Karuku <peter.karuku@student.moringaschool.com>
2021-09-10 15:21:03 +0300

    add century function

--- js/script.js
+++ js/script.js
@@ -9,7 +9,17 @@ frm.addEventListener("submit", function(e){
     e.preventDefault();
     let frmakan =new FormData(this);
 
-    
+    function centuryFromYear(year) {
+        if(typeof year == 'string')
+          if(year.toString().slice(-2) == '00')
+            return year.toString().slice(0,2);
+          else
+            return (Math.floor(+year/100) +1).toString();
+        else if(typeof year == 'number')
+          return Math.floor((year-1)/100) + 1;
+        else
+          return undefined;
+        }
 
     const YY = parseInt(frmakan.get("year"));
     const CC = centuryFromYear(frmakan.get("year"));
